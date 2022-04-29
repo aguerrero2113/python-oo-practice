9d807d732260d99611ccb266474a68af2fffaced
Artemio Guerrero <aguerrero2113@gmail.com>
2022-04-29 16:06:30 -0700

    modified python  files and txt files.

--- serial.py
+++ serial.py
@@ -19,4 +19,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self,start = 0):
 
+        self.start = self.start = start
+
+    def __repr__(self):
+
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        
+        self.next = self.start
